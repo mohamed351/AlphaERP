40d697558775ad2af0394eed0e5a7c73cfac2359
Mohamed beshri Amer <mohamed.perry351@gmail.com>
2021-01-16 20:30:25 +0200

    BarCode Validation in Angular Async Validation and FormValition

--- ClientApp/src/app/products/product.custome.validation.ts
+++ ClientApp/src/app/products/product.custome.validation.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { AsyncValidatorFn, ValidationErrors , AbstractControl} from "@angular/forms";
+import { AsyncValidatorFn, ValidationErrors , AbstractControl, FormGroup, FormArray} from "@angular/forms";
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
 
@@ -43,8 +43,52 @@ export class ProductCustomeValidation {
     }
 
     }
-    ValidateBarCode(){
+    ValidateBarCode(id):AsyncValidatorFn{
+        return (AbstractControl:AbstractControl):Promise<ValidationErrors|null>
+        |Observable<ValidationErrors |null> =>{
+            if(id == null){
+            return this.http.get(`/api/Products/validateBarcode/${AbstractControl.value}`).pipe(
+                map(a=>{
+                    if(a == true){
+                        return null
+                    }
+                    else{
+                        AbstractControl.hasError("Bar Code is Duplicated");
+                        return { Unique: "Product BarCode Should be unique" };
+                    }
+                })
+            );
+            }
+            else{
 
+                return this.http.get(`/api/Products/validateBarcode/${AbstractControl.value}?ID=${id}`).pipe(
+                    map(a=>{
+                        if(a == true){
+                            return null
+                        }
+                        else{
+                            AbstractControl.hasError("Bar Code is Duplicated");
+                            return { Unique: "Product BarCode Should be unique" };
+                        }
+                    })
+                );
+
+            }
+        }
+    }
+
+    ValidationbarCodeForm(FormArray:FormArray):ValidationErrors{
+        return (AbstractControl:AbstractControl):ValidationErrors|null =>{
+            FormArray.controls.forEach(element => {
+                let controlString = (<String>AbstractControl.value).trim();
+                if(element.get("barcode").value == controlString){
+                    AbstractControl.hasError("Bar Code is Duplicated");
+                    return { Unique: "ProductName Should be unique" };
+                }
+                
+           });
+           return null;
+        }
     }
 
 
